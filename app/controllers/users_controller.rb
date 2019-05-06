cc8d0a03be04dce6fd8d3ee3a5c6f1bb9cecb707
mgmmy <mgmilk.u@gmail.com>
2019-05-06 18:34:24 +0900

    Update form.haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index
+  end
+
   def edit
   end
 
@@ -7,6 +10,14 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
     end
   end
 
