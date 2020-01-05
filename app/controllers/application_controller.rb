b4ef23c48a744ef073e3e2abc259fd0dea89e09b
megumi_ushijima <megumi.ushijima@icare.jpn.com>
2020-01-05 11:17:53 +0900

    記述修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  protect_from_forgery with: :exception
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protect_from_forgery with: :exception
 
   protected
 
