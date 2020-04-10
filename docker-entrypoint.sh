dfbc30a63ee3b4eff4dddbc6a89f45d4e9f8ffaa
Yasuda Toshiyuki <toshiyuki.yasuda@icare.jpn.com>
2020-04-10 09:33:19 +0900

    docker-compose.ymlにdocker-entrypoint.shの定義がなく、docker-entrypoint.shが動いていなかったので、定義を追加

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,10 @@
 
 set -e
 
+# https://robots.thoughtbot.com/parallel-gem-installing-using-bundler
+number_of_cores=`nproc`
+bundle check || bundle install -j"$number_of_cores"  --binstubs="$BUNDLE_BIN"
+
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
